026a4b97b81936f1c0119f0eac0743dd19ff9b95
arnoldc <arnold.camas@gmail.com>
2023-03-30 21:45:45 +0800

    nested suspense

--- src/components/users/UserDetails.tsx
+++ src/components/users/UserDetails.tsx
@@ -1,5 +1,7 @@
+import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { ErrorFallback } from '../ErrorFallback';
+import { Loading } from '../Loading';
 import { AccountDetails } from './AccountDetails';
 import { MovieDetails } from './MovieDetails';
 
@@ -10,15 +12,17 @@ interface Props {
 export function UserDetails({ userId, movieId }: Props) {
   return (
     <div>
-      <h4 className="text-center mt-5">User details</h4>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <AccountDetails userId={userId} />
-      </ErrorBoundary>
+      <Suspense fallback={<Loading />}>
+        <h4 className="text-center mt-5">User details</h4>
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <AccountDetails userId={userId} />
+        </ErrorBoundary>
 
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <h4 className="text-center mt-5">Favorite movie</h4>
-      </ErrorBoundary>
-      <MovieDetails movieId={movieId} />
+        <ErrorBoundary FallbackComponent={ErrorFallback}>
+          <h4 className="text-center mt-5">Favorite movie</h4>
+        </ErrorBoundary>
+        <MovieDetails movieId={movieId} />
+      </Suspense>
     </div>
   );
 }
